7a3d4f1e905db9d523d8406673677b7a41de58b9
Altairian7 <artisharma.as12.as@gmail.com>
2024-12-02 21:57:32 +0530

    added types in package-json and added 4 cubes in appJSX

--- r3f_prototype/src/App.jsx
+++ r3f_prototype/src/App.jsx
@@ -1,14 +1,36 @@
-import './App.css'
+import "./App.css";
+import { Canvas } from "@react-three/fiber"
 
-function App() {
 
+
+const App = () => {
   return (
-    <>
-      <h1>
-        Hi Mom
-      </h1>
-    </>
+    <Canvas>
+      <directionalLight position={[0,0,2]} />
+
+      <mesh position={[2,0,0]}>
+        <boxGeometry  />
+        <meshStandardMaterial color={"red"} />
+      </mesh>
+
+      <mesh position={[-2,0,0]}>
+        <boxGeometry  />
+        <meshStandardMaterial color={"blue"} />
+      </mesh>
+
+      <mesh position={[0,-2,0]}>
+        <boxGeometry  />
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+      
+      <mesh position={[0,2,0]}>
+        <boxGeometry  />
+        <meshStandardMaterial color={"green"} />
+      </mesh>
+      
+    </Canvas>
   )
 }
 
-export default App
+export default App;
+
