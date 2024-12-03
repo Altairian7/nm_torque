5e5a4669b862d3504ef6f0059e6ada9560168813
Altairian7 <artisharma.as12.as@gmail.com>
2024-12-03 19:24:34 +0530

    added sphere to the canvas

--- r3f_prototype/src/App.jsx
+++ r3f_prototype/src/App.jsx
@@ -1,27 +1,46 @@
 import "./App.css";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { WireframeGeometry } from "three";
 
 const Cube = ({ position, size, color }) => {
   const ref = useRef();
 
   useFrame((state, delta) => {
+
     ref.current.rotation.x += delta
+    ref.current.rotation.y += delta
+    ref.current.rotation.z += delta * 2
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 4
+    console.log(state.clock.elapsedTime)
+
   });
 
   return (
-    <mesh position={position} ref={ref}>
-      <boxGeometry args={[size]} />
+  <mesh position={position} ref={ref}>
+      <boxGeometry args={size} />
+      {/* <wireframeGeometry args={size} /> */}
+      {/* <icosahedronGeometry args={[size]}/> */}
       <meshStandardMaterial color={color} />
     </mesh>
   );
 };
 
+
+const Sphere = ({ position, size, color }) => {
+  return (
+    <mesh position={position}>
+      <sphereGeometry args={size} />
+      <meshStandardMaterial color={color}/>
+    </mesh>
+  )
+}
+
 const App = () => {
   return (
     <Canvas>
-      {/* <directionalLight position={[0, 0, 2]} /> */}
-      <ambientLight />
+      <directionalLight position={[0, 0, 2]} />
+      {/* <ambientLight /> */}
 
       {/* <group position={[0,-1,0]}>
         <Cube position={[1, 0, 0]} size color={"red"} />
@@ -30,8 +49,9 @@ const App = () => {
         <Cube position={[1, 2, 0]} size color={"yellow"} />
       </group> */}
 
-      <Cube position={[0, 0, 0]} color={"red"} />
-
+      {/* <Cube position={[0, 0, 0]} args={[1,12]} color={"gold"} /> */}
+      <Sphere position={[0,0,0]} args={[1, 30, 30]} color={"red"} />
+ 
     </Canvas>
   );
 };
