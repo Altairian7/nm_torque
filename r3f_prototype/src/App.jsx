b41040303bf1e2d04c531562bfc4df5305f05a94
Altairian7 <artisharma.as12.as@gmail.com>
2024-12-03 18:49:08 +0530

    created a cube component and reused it 4 times

--- r3f_prototype/src/App.jsx
+++ r3f_prototype/src/App.jsx
@@ -1,36 +1,28 @@
 import "./App.css";
-import { Canvas } from "@react-three/fiber"
+import { Canvas } from "@react-three/fiber";
 
-
+const Cube = ({ position, size, color}) => {
+  return (
+    <mesh position={position}>
+      <boxGeometry args={[size]}/>
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
 
 const App = () => {
   return (
     <Canvas>
-      <directionalLight position={[0,0,2]} />
+      <directionalLight position={[0, 0, 2]} />
 
-      <mesh position={[2,0,0]}>
-        <boxGeometry  />
-        <meshStandardMaterial color={"red"} />
-      </mesh>
+      <Cube position={[2,0,0]}  size color={"red"}  />
+      <Cube position={[-2,0,0]}  size color={"blue"}  />
+      <Cube position={[0,-2,0]}  size color={"pink"}  />
+      <Cube position={[0,2,0]}  size color={"yellow"}  />
 
-      <mesh position={[-2,0,0]}>
-        <boxGeometry  />
-        <meshStandardMaterial color={"blue"} />
-      </mesh>
-
-      <mesh position={[0,-2,0]}>
-        <boxGeometry  />
-        <meshStandardMaterial color={"orange"} />
-      </mesh>
-      
-      <mesh position={[0,2,0]}>
-        <boxGeometry  />
-        <meshStandardMaterial color={"green"} />
-      </mesh>
-      
+    
     </Canvas>
-  )
-}
+  );
+};
 
 export default App;
-
