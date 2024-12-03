ba0224005c89091032e2a831bf800bdd836b29e5
Altairian7 <artisharma.as12.as@gmail.com>
2024-12-03 18:54:56 +0530

    added lights and grouped all cubes together

--- r3f_prototype/src/App.jsx
+++ r3f_prototype/src/App.jsx
@@ -1,10 +1,11 @@
+import { AmbientLight } from "three";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 
-const Cube = ({ position, size, color}) => {
+const Cube = ({ position, size, color }) => {
   return (
     <mesh position={position}>
-      <boxGeometry args={[size]}/>
+      <boxGeometry args={[size]} />
       <meshStandardMaterial color={color} />
     </mesh>
   );
@@ -14,13 +15,14 @@ const App = () => {
   return (
     <Canvas>
       <directionalLight position={[0, 0, 2]} />
+      {/* <ambientLight /> */}
 
-      <Cube position={[2,0,0]}  size color={"red"}  />
-      <Cube position={[-2,0,0]}  size color={"blue"}  />
-      <Cube position={[0,-2,0]}  size color={"pink"}  />
-      <Cube position={[0,2,0]}  size color={"yellow"}  />
-
-    
+      <group position={[0,-1,0]}>
+        <Cube position={[1, 0, 0]} size color={"red"} />
+        <Cube position={[-1, 0, 0]} size color={"blue"} />
+        <Cube position={[-1, 2, 0]} size color={"pink"} />
+        <Cube position={[1, 2, 0]} size color={"yellow"} />
+      </group>
     </Canvas>
   );
 };
